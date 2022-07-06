2c7117268a4d3c28d14b5563e7b1a4d47f4d3230
Andrés Tapiero <andrestapiero72@gmail.com>
2022-07-06 15:40:14 -0500

    esto es un cambio de sebas

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
 
         System.out.println("Esto es nuestra tesis no modificar mucho");
+        System.out.println("Yo soy Sebas y me caes bien!");
     }
 }
