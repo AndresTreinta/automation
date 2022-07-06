416387e3398ac362ddbafe708737251ac6122477
Andrés Tapiero <andrestapiero72@gmail.com>
2022-07-06 15:44:41 -0500

    fix comment Andres

--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ public class main {
     public static void main(String[] args) {
 
         System.out.println("Esto es nuestra tesis no modificar mucho");
-        System.out.println("Yo soy Sebas y me caes bien!");
+        System.out.println("Yo soy Treinta y me caes bien!");
     }
 }
