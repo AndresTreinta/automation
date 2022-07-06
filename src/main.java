364d31fa12e015b7486e9f66fb0959eec7a50b38
Andrés Tapiero <andrestapiero72@gmail.com>
2022-07-06 15:37:30 -0500

    rename text

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("Hola ya son las 3 de la tarde");
+        System.out.println("Esto es nuestra tesis no modificar mucho");
     }
 }
